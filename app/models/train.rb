fadacc507cb2627a9216cb229a4799d19b1770b7
Hayri Cicek <hayri@hoshilab.com>
2014-10-08 12:35:36 +0200

    Update train.rb

--- app/models/train.rb
+++ app/models/train.rb
@@ -5,10 +5,10 @@ class Train < ActiveRecord::Base
   validates :date_of_travel, presence: true
 
   def self.search(search)
-		if search
-			where(['travel_from LIKE ? OR destination LIKE ?', "%#{search}%","%#{search}%"])
-		else
-		  all	
-		end
-	end
+    if search
+      where(['travel_from LIKE ? OR destination LIKE ?', "%#{search}%","%#{search}%"])
+    else
+      all	
+    end
+  end
 end
